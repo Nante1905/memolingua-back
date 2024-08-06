b67a378ea7e342e4fab0508c3c02974dfe0ade16
Nante <nantemino15@gmail.com>
2024-08-06 14:26:48 +0300

    feat: [AUTH] add refresh token feature

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,12 +1,17 @@
 import { configDotenv } from "dotenv";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as jwt from "jsonwebtoken";
 import { EntityNotFoundError } from "typeorm";
 import { User } from "../../../database/entities/User";
+import { AuthController } from "../controllers/auth.controller";
 import { AuthCredentials } from "../types/auth.type";
 
 configDotenv();
 
-const generateJWT = (user: User, role: string) => {
+export const refreshTokens: Record<string, any> = {};
+
+export const generateJWT = (user: User, role: string) => {
   return jwt.sign(
     {
       user: user.id,
@@ -14,8 +19,16 @@ const generateJWT = (user: User, role: string) => {
       role: role,
     },
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    { expiresIn: "1m" }
+    // { expiresIn: "5s" }
   );
+};
+
+export const generateRefreshToken = (user: User) => {
+  return jwt.sign({}, process.env.JWT_SECRET, {
+    expiresIn: "2d",
+    subject: user.id,
+  });
 };
 
 export const logIn = async (credentials: AuthCredentials, role: string) => {
@@ -26,7 +39,12 @@ export const logIn = async (credentials: AuthCredentials, role: string) => {
   if (user.length == 0) {
     throw new EntityNotFoundError(User, "");
   }
-  return generateJWT(user[0], role);
+
+  const refreshToken = generateRefreshToken(user[0]);
+  !refreshTokens[user[0].id] && (refreshTokens[user[0].id] = new Set());
+  refreshTokens[user[0].id].add(refreshToken);
+
+  return [generateJWT(user[0], role), refreshToken];
 };
 
 export const signup = async (user: User) => {
@@ -101,5 +119,56 @@ export const updatePassword = async (
     );
   } else {
     throw new EntityNotFoundError(User, "");
+  }
+};
+
+export const validateRefreshToken = async (
+  req: Request,
+  res: Response,
+  token: string
+) => {
+  try {
+    // if (!token) {
+    //   res
+    //     .json({ ok: false, error: "Invalid token" })
+    //     .status(StatusCodes.BAD_REQUEST);
+    //   return;
+    // }
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+      console.log(refreshTokens);
+
+      if (err) {
+        res
+          .json({ ok: false, error: "Invalid token" })
+          .status(StatusCodes.BAD_REQUEST);
+        return;
+      }
+      if (
+        !refreshTokens[decoded.sub as string] ||
+        !refreshTokens[decoded.sub as string].has(token)
+      ) {
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ ok: false, error: "Invalid refresh token" });
+        return;
+      }
+
+      refreshTokens[decoded.sub as string].delete(token);
+
+      const user = await User.findOne({
+        where: { id: decoded.sub as string },
+        relations: { role: true },
+      });
+      if (!user) {
+        throw new EntityNotFoundError(User, "");
+      }
+
+      AuthController.generateAccessToken(req, res, user);
+      return;
+    });
+  } catch (error) {
+    // throw error;
+    console.log(error);
   }
 };
